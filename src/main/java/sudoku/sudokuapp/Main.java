c6cb25f668becfd2c8d1b60b0bb68e49382f68f3
james <jsumma3167@westfield.ma.edu>
2022-06-18 13:51:42 -0400

    updates

--- src/main/java/sudoku/sudokuapp/Main.java
+++ src/main/java/sudoku/sudokuapp/Main.java
@@ -1,6 +1,8 @@
 package sudoku.sudokuapp;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +12,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
