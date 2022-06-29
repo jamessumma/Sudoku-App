501d3f4a0b69e57b7725818536a30fdcc3e9932a
james <jsumma3167@westfield.ma.edu>
2022-06-29 12:27:54 -0400

    Main menu

--- src/main/java/sudoku/sudokuapp/PrimaryController.java
+++ src/main/java/sudoku/sudokuapp/PrimaryController.java
@@ -1,4 +1,18 @@
 package sudoku.sudokuapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class PrimaryController {
+    @FXML
+    private Label title;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Button startButton;
+
 }
