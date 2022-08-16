3c87ef68b2aa91e898b02b1036c7263066149a31
james <jsumma3167@westfield.ma.edu>
2022-08-16 12:25:40 -0400

    new method of game generation, other one was inefficient

--- src/main/java/sudoku/sudokuapp/Game.java
+++ src/main/java/sudoku/sudokuapp/Game.java
@@ -9,14 +9,17 @@ public class Game {
     int[][] board = new int[9][9];
 
     public Game() {
+        clearBoard();
+        firstRowGenerator();
+        fullGenerator();
+
+    }
+    private void clearBoard(){
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 board[i][j] = 0;
             }
         }
-        firstRowGenerator();
-        fullGenerator();
-
     }
 
 
@@ -32,29 +35,72 @@ public class Game {
     }
 
     private void fullGenerator(){
-        int maxAttempts = 1000;
-        int min = 1;
-        int max = 9;
-        int randomNum;
-
-        for (int i = 1; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                int k = 0;
-                while (maxAttempts > k){
-                    randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
-                    if (!rowContains(i, randomNum) && !columnContains(j, randomNum)){
-                        board[i][j] = randomNum;
-                        break;
+        ArrayList nums = new ArrayList();
+        for (int i = 1; i < 10; i++) {
+            nums.add(i);
+        }
+        int maxAttempts = 20000000;
+        int tries = 0;
+        while (maxAttempts > tries) {
+            clearBoard();
+            firstRowGenerator();
+            for (int i = 1; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    Collections.shuffle(nums);
+                    for (int k = 0; k < nums.size(); k++) {
+                        if (!rowContains(i, (int) nums.get(k)) && !columnContains(j, (int) nums.get(k))) {
+                            board[i][j] = (int) nums.get(k);
+                            break;
+                        }
+                        if (k == nums.size() - 1) {
+                            i = 8;
+                            j = 8;
+                            tries++;
+                        }
                     }
-                    k++;
                 }
             }
         }
-
         //int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
 
     }
 
+    private boolean entryIsValid(int i, int j, int num){
+
+        return true;
+    }
+
+    // box num is in order, left to right, top to bottom
+    // box 1 is top left, box 9 is bottom right
+    // box 3 is top right, box 7 is bottom left
+    private int getBoxNum(int i, int j){
+        if (i <= 3){
+            if (j <= 3) return 1;
+            if (j <= 6) return 2;
+            else return 3;
+        } else if (i <= 6){
+            if (j <= 3) return 4;
+            if (j <= 6) return 5;
+            else return 6;
+        } else {
+            if (j <= 3) return 7;
+            if (j <= 6) return 8;
+            else return 9;
+        }
+    }
+
+    // returns true if the box contains the number
+    private boolean boxContains(int rowStart, int columnStart, int num){
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[rowStart + i][columnStart + j] == num) {
+                    return true;
+                }
+
+        return false;
+    }
+
+    // returns true if the row contains the number
     private Boolean rowContains(int rowIndex, int num){
         for (int i = 0; i < 9; i++) {
             if (board[rowIndex][i] == num) return true;
